a5139335312576d8453d11fed97aed78b68f7c40
learngeetha <learn.geetha@gmail.com>
2026-03-12 01:01:49 +0530

    Add HTTP server to respond with greeting message
    Implemented a simple HTTP server that responds with a greeting.

--- JavaApp.java
+++ JavaApp.java
@@ -1,5 +1,21 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class JavaApp {
-    public static void main(String[] args) {
-        System.out.println("Hello from Cloud Run");
+    public static void main(String[] args) throws Exception {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "Hello from Cloud Run!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.start();
+        System.out.println("Server started on port 8080");
     }
 }
